ccb41bb6926806cb71a2c76a7d5608cb362f87d1
JiangGuangqing <hx@aumc.cc>
2020-02-25 08:40:47 +0000

    用户界面 - 商品筛选排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -10,10 +11,41 @@ class ProductsController extends Controller
     //
     public function index(Request $request)
     {
-        $products = Product::query()
-            ->where('on_sale', true)
-            ->paginate(16);
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', ['products' => $products]);
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+
+            $builder->where(function (Builder $query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function (Builder $query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+        //是否有提交 order 参数，如果有救赋值给$order 变量
+        //order  参数用来控制商品的排序规则
+
+        if ($order = $request->input('order', '')) {
+            //是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                //如果字符串的开头是这 3 个字符之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order,
+            ],
+        ]);
     }
 }
