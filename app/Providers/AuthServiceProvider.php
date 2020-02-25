8f08150919cf3badafda6450a65e0e494ab34c5f
JiangGuangqing <hx@aumc.cc>
2020-02-25 04:01:14 +0000

    初始化管理后台

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
-        Gate::guessPolicyNamesUsing(function ($class) {
+        \Gate::guessPolicyNamesUsing(function ($class) {
             return '\\App\\Policies\\' . class_basename($class) . 'Policy';
         });
     }
