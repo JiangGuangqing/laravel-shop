6245a42e1b090e22dc4b858ccd95cd05ce98a269
JiangGuangqing <hx@aumc.cc>
2020-02-27 05:21:29 +0000

    管理后台- 订单列表

---
+++ app/Admin/Controllers/OrdersController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Order;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class OrdersController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = '订单';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Order());
+
+        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
+
+        $grid->no('订单流水号');
+        $grid->column('user.name', '买家');
+        $grid->total_amount('总金额')->sortable();
+        $grid->paid_at('支付时间')->sortable();
+        $grid->ship_status('物流')->display(function ($value) {
+            return Order::$shipStatusMap[$value];
+        });
+        $grid->refund_status('退款状态')->display(function ($value) {
+            return Order::$refundStatusMap[$value];
+        });
+
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            $actions->disableDelete();
+            $actions->disableEdit();
+        });
+
+        $grid->tools(function ($tools) {
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+        return $grid;
+    }
+}
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,17 +18,17 @@ class Order extends Model
     const SHIP_STATUS_RECEIVED = 'received';
 
     public static $refundStatusMap = [
-        self:: REFUND_STATUS_PENDING => 'pending',
-        self:: REFUND_STATUS_APPLIED => 'applied',
-        self:: REFUND_STATUS_PROCESSING => 'processing',
-        self:: REFUND_STATUS_SUCCESS => 'success',
-        self:: REFUND_STATUS_FAILED => 'failed',
+        self:: REFUND_STATUS_PENDING => '未退款',
+        self:: REFUND_STATUS_APPLIED => '已申请退款',
+        self:: REFUND_STATUS_PROCESSING => '退款中',
+        self:: REFUND_STATUS_SUCCESS => '退款成功',
+        self:: REFUND_STATUS_FAILED => '退款失败',
     ];
 
     public static $shipStatusMap = [
-        self:: SHIP_STATUS_PENDING => 'pending',
-        self:: SHIP_STATUS_DELIVERED => 'delivered',
-        self:: SHIP_STATUS_RECEIVED => 'received',
+        self:: SHIP_STATUS_PENDING => '未发货',
+        self:: SHIP_STATUS_DELIVERED => '已发货',
+        self:: SHIP_STATUS_RECEIVED => '已收货',
     ];
 
     protected $fillable = [
