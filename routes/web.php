84a6a528803c3164fb1e97f2a6f101485b24946f
JiangGuangqing <hx@aumc.cc>
2020-02-27 09:09:43 +0000

    用户界面 申请退款

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,8 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
 
+    Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
